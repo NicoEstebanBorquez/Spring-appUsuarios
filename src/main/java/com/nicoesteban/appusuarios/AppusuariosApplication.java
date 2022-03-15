5884d59244b71c43c9b6be75fec2d5e4dd3d4855
NicoEstebanBorquez <nico.esteban.borquez@gmail.com>
2022-03-15 19:58:09 +0100

    Inico de proyecto

---
+++ src/main/java/com/nicoesteban/appusuarios/AppusuariosApplication.java
@@ -0,0 +1,13 @@
+package com.nicoesteban.appusuarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppusuariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppusuariosApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/nicoesteban/appusuarios/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.nicoesteban.appusuarios;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(AppusuariosApplication.class);
+	}
+
+}
